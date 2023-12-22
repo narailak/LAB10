79a01877bf2faf174f1d874619b7584b29bfd43c
OATA <narailak_s@cmu.ac.th>
2023-12-22 12:17:28 +0700

    finish

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,18 +1,23 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
+int main()
+{
+	string textline;
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
-    source.close();
-    dest.close();
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while (getline(source, textline))
+	{
+		dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
+	source.close();
+	dest.close();
 	return 0;
 }
